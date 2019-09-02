95f507ae154b32d3d6b179a0fb847144c77778f7
egoing <egoing@gmail.com>
2019-09-03 02:27:41 +0900

    6.4. 문자열의 표현

---
+++ Data_and_operation/StringApp.java
@@ -0,0 +1,21 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		
+		// Character VS String 
+		System.out.println("Hello World"); // String
+		System.out.println('H'); // Character
+		System.out.println("H"); 
+	
+		System.out.println("Hello "
+				+ "World");
+		
+		// new line
+		System.out.println("Hello \nWorld");
+		
+		// escape
+		System.out.println("Hello \"World\"");// Hello "World"
+	}
+
+}
