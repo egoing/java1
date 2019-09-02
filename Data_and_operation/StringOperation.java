761285ab781e2198dec3f1a41ba629eb5f557d7d
egoing <egoing@gmail.com>
2019-09-03 02:37:33 +0900

    6.5. 문자열의 처리

---
+++ Data_and_operation/StringOperation.java
@@ -0,0 +1,11 @@
+
+public class StringOperation {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World".length()); // 11
+		System.out.println("Hello, [[[name]]] ... bye. ".replace("[[[name]]]", "duru"));
+
+	}
+
+}
