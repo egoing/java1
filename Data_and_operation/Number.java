bf963517df4ecd9378c1b800e784bde21c11a5cf
egoing <egoing@gmail.com>
2019-09-03 01:32:21 +0900

    6.3. 숫자와 연산

---
+++ Data_and_operation/Number.java
@@ -0,0 +1,18 @@
+
+public class Number {
+
+	public static void main(String[] args) {
+		// Operator
+		System.out.println(6 + 2); // 8
+		System.out.println(6 - 2); // 4
+		System.out.println(6 * 2); // 12
+		System.out.println(6 / 2); // 3
+
+		System.out.println(Math.PI); // 3.141592653589793
+		System.out.println(Math.floor(Math.PI));
+		System.out.println(Math.ceil(Math.PI));
+		
+		
+	}
+
+}
