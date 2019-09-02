00930b942166ea34f4b7ca87314d98b015bce627
egoing <egoing@gmail.com>
2019-09-02 23:16:55 +0900

    6.2. 데이터 타입

---
+++ Data_and_operation/Datatype.java
@@ -0,0 +1,17 @@
+public class Datatype{
+	public static void main(String[] args) {
+		System.out.println(6); // Number
+		System.out.println("six"); // String
+		
+		System.out.println("6"); // String 6
+		
+		System.out.println(6+6); // 12
+		System.out.println("6"+"6"); // 66
+		
+		System.out.println(6*6); // 36
+//		System.out.println("6"*"6");
+		
+		System.out.println("1111".length()); // 4
+//		System.out.println(1111.length());
+	}
+}
