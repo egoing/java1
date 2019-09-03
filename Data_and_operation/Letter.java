de68aaf23918a743d715869683b0599e0bbabf81
egoing <egoing@gmail.com>
2019-09-03 18:10:55 +0900

    8.2. 변수의 효용

---
+++ Data_and_operation/Letter.java
@@ -0,0 +1,12 @@
+
+public class Letter {
+
+	public static void main(String[] args) {
+		String name = "leezche";
+		System.out.println("Hello, "+name+" ... "+name+" ... egoing ... bye");
+		
+		double VAT = 10.0;
+		System.out.println(VAT);
+	}
+
+}
