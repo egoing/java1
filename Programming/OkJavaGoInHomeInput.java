d65cb2fe6e58fd3dc8c97a84a5763ab094ff8d1d
egoing <egoing@gmail.com>
2019-09-04 02:56:44 +0900

    11. 입력과 출력

---
+++ Programming/OkJavaGoInHomeInput.java
@@ -0,0 +1,29 @@
+import org.opentutorials.iot.Elevator;
+import org.opentutorials.iot.Lighting;
+import org.opentutorials.iot.Security;
+
+public class OkJavaGoInHomeInput {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Country : "+args[1]);
+		String id = args[0];
+		
+		// Elevator call 
+		Elevator myElevator = new Elevator(id);
+		myElevator.callForUp(1);
+		
+		// Security off 
+		Security mySecurity = new Security(id);
+		mySecurity.off();
+		
+		// Light on
+		Lighting hallLamp = new Lighting(id+" / Hall Lamp");
+		hallLamp.on();
+		
+		Lighting floorLamp = new Lighting(id+" / floorLamp");
+		floorLamp.on();
+
+	}
+
+}
