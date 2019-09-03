ab61c2c53ad2e143f953533ef91a945e0a545954
egoing <egoing@gmail.com>
2019-09-03 18:53:21 +0900

    8.3. 데이터 타입의 변환 - 캐스팅

---
+++ Data_and_operation/Casting.java
@@ -0,0 +1,24 @@
+
+public class Casting {
+
+	public static void main(String[] args) {
+		
+		double a = 1.1;
+		double b = 1;
+		double b2 = (double) 1;
+		
+		System.out.println(b);
+		
+		// int c = 1.1;
+		double d = 1.1;
+		int e = (int) 1.1;
+		System.out.println(e);
+		
+		// 1 to String 
+		String f = Integer.toString(1);
+		System.out.println(f.getClass());
+
+
+	}
+
+}
