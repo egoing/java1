4123ca9efd419e5545e2a0692de78cfa2c0ca2af
egoing <egoing@gmail.com>
2019-09-03 17:38:08 +0900

    8.1. 변수의 정의

---
+++ Data_and_operation/Variable.java
@@ -0,0 +1,16 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		
+		int a = 1; // Number -> integer  ... -2, -1 , 0, 1, 2 ...
+		System.out.println(a);
+		
+		double b = 1.1; // real number -> double ... -2.0, -1.0, 0, 1.0, 2.0 ...
+		System.out.println(b);
+		
+		String c = "Hello World";
+		System.out.println(c);
+	}
+
+}
