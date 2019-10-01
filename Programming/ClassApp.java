455c4a23b72073a6c7790b25cff55f5d70c074e8
egoing <egoing@gmail.com>
2019-10-01 16:08:51 +0900

    13.3. 자바 문서 보는 법 - 클래스

---
+++ Programming/ClassApp.java
@@ -0,0 +1,12 @@
+
+public class ClassApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Math.PI);
+		System.out.println(Math.floor(1.6));
+		System.out.println(Math.ceil(1.6));
+
+	}
+
+}
