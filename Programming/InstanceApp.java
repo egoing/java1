136c6734246880effacad2d55769b0e14452c558
egoing <egoing@gmail.com>
2019-10-01 21:13:45 +0900

    13.4. 자바 문서 보는 법 - 인스턴스

---
+++ Programming/InstanceApp.java
@@ -0,0 +1,32 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+public class InstanceApp {
+
+	public static void main(String[] args) throws IOException{
+		
+		PrintWriter p1 = new PrintWriter("result1.txt");
+		p1.write("Hello 1");
+		p1.close();
+		
+		PrintWriter p2 = new PrintWriter("result2.txt");
+		p2.write("Hello 2");
+		p2.close();
+		
+//		PrintWriter.write("result1.txt", "Hello 1");	
+//		PrintWriter.write("result2.txt", "Hello 2");
+//		PrintWriter.write("result1.txt", "Hello 1");	
+//		PrintWriter.write("result2.txt", "Hello 2");
+//		PrintWriter.write("result1.txt", "Hello 1");	
+//		PrintWriter.write("result2.txt", "Hello 2");
+//		PrintWriter.write("result1.txt", "Hello 1");	
+//		PrintWriter.write("result2.txt", "Hello 2");
+//		PrintWriter.write("result1.txt", "Hello 1");	
+//		PrintWriter.write("result2.txt", "Hello 2");
+//		PrintWriter.write("result1.txt", "Hello 1");	
+//		PrintWriter.write("result2.txt", "Hello 2");
+//		PrintWriter.write("result1.txt", "Hello 1");	
+//		PrintWriter.write("result2.txt", "Hello 2");
+	}
+
+}
