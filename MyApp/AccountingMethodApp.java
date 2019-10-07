29fa938baed72267a7f6eb5fc5cdf75ec3975324
egoing <egoing@gmail.com>
2019-10-07 21:40:52 +0900

    14.9. 나의 앱 만들기 - 메소드

---
+++ MyApp/AccountingMethodApp.java
@@ -0,0 +1,50 @@
+
+public class AccountingMethodApp {
+	public static double valueOfSupply;
+	public static double vatRate;
+	public static double expenseRate;
+	public static void main(String[] args) {
+		valueOfSupply = 10000.0;
+		vatRate = 0.1;
+		expenseRate = 0.3;
+		print();
+	}
+
+	public static void print() {
+		System.out.println("Value of supply : " + valueOfSupply);
+		System.out.println("VAT : " + getVAT());
+		System.out.println("Total : " + getTotal());
+		System.out.println("Expense : " + getExpense());
+		System.out.println("Income : " + getIncome());
+		System.out.println("Dividend 1 : " + getDiviend1());
+		System.out.println("Dividend 2 : " + getDiviend2());
+		System.out.println("Dividend 3 : " + getDiviend3());
+	}
+
+	public static double getDiviend1() {
+		return getIncome() * 0.5;
+	}
+	public static double getDiviend2() {
+		return getIncome() * 0.3;
+	}
+	public static double getDiviend3() {
+		return getIncome() * 0.2;
+	}
+
+	public static double getIncome() {
+		return valueOfSupply - getExpense();
+	}
+
+	public static double getExpense() {
+		return valueOfSupply * expenseRate;
+	}
+
+	public static double getTotal() {
+		return valueOfSupply + getVAT();
+	} 
+
+	public static double getVAT() {
+		return valueOfSupply * vatRate;
+	}
+
+}
