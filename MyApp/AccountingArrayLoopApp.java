11a7f65d7e209b37f538de18130fe7a4e8b13443
egoing <egoing@gmail.com>
2019-10-07 15:26:54 +0900

    14.8. 나의 앱 만들기 - 반복문의 도입

---
+++ MyApp/AccountingArrayLoopApp.java
@@ -0,0 +1,37 @@
+
+public class AccountingArrayLoopApp {
+
+	public static void main(String[] args) {
+
+		double valueOfSupply = Double.parseDouble(args[0]);
+		double vatRate = 0.1;
+		double expenseRate = 0.3;
+		double vat = valueOfSupply * vatRate;
+		double total = valueOfSupply + vat;
+		double expense = valueOfSupply * expenseRate;
+		double income = valueOfSupply - expense;
+		
+		
+
+		System.out.println("Value of supply : " + valueOfSupply);
+		System.out.println("VAT : " + vat);
+		System.out.println("Total : " + total);
+		System.out.println("Expense : " + expense);
+		System.out.println("Income : " + income);
+		
+		double[] dividendRates = new double[3];
+		dividendRates[0] = 0.5;
+		dividendRates[1] = 0.3;
+		dividendRates[2] = 0.2;
+		
+			
+		int i = 0;
+		while(i < dividendRates.length) {
+			System.out.println("Dividend : " + (income*dividendRates[i]) );
+			i = i + 1;
+		}
+		
+
+	}
+
+}
