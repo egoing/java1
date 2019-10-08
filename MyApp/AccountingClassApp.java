aafc9c1efe10ef1567257c95d40db79c71723196
egoing <egoing@gmail.com>
2019-10-08 09:16:04 +0900

    14.11. 나의 앱 만들기 - 인스턴스

--- MyApp/AccountingClassApp.java
+++ MyApp/AccountingClassApp.java
@@ -1,8 +1,8 @@
 class Accounting{
-	public static double valueOfSupply;
-	public static double vatRate;
-	public static double expenseRate;
-	public static void print() {
+	public double valueOfSupply;
+	public double vatRate;
+	public double expenseRate;
+	public void print() {
 		System.out.println("Value of supply : " + valueOfSupply);
 		System.out.println("VAT : " + getVAT());
 		System.out.println("Total : " + getTotal());
@@ -13,43 +13,58 @@ class Accounting{
 		System.out.println("Dividend 3 : " + getDiviend3());
 	}
 
-	public static double getDiviend1() {
+	public double getDiviend1() {
 		return getIncome() * 0.5;
 	}
-	public static double getDiviend2() {
+	public double getDiviend2() {
 		return getIncome() * 0.3;
 	}
-	public static double getDiviend3() {
+	public double getDiviend3() {
 		return getIncome() * 0.2;
 	}
 
-	public static double getIncome() {
+	public double getIncome() {
 		return valueOfSupply - getExpense();
 	}
 
-	public static double getExpense() {
+	public double getExpense() {
 		return valueOfSupply * expenseRate;
 	}
 
-	public static double getTotal() {
+	public double getTotal() {
 		return valueOfSupply + getVAT();
 	} 
 
-	public static double getVAT() {
+	public double getVAT() {
 		return valueOfSupply * vatRate;
 	}
 }
 public class AccountingClassApp {
 	
 	public static void main(String[] args) {
-		Accounting.valueOfSupply = 10000.0;
-		Accounting.vatRate = 0.1;
-		Accounting.expenseRate = 0.3;
-		Accounting.print();
-		// anotherVariable = ...;
-		// anotherMethod = ...;
+		// instance 
+		Accounting a1 = new Accounting();
+		a1.valueOfSupply = 10000.0;
+		a1.vatRate = 0.1;
+		a1.expenseRate = 0.3;
+		a1.print();
+		
+		Accounting a2 = new Accounting();
+		a2.valueOfSupply = 20000.0;
+		a2.vatRate = 0.05;
+		a2.expenseRate = 0.2;
+		a2.print();
+		
+		a1.print();
 	}
-
-	
-
 }
+
+
+
+
+
+
+
+
+
+
